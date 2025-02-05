64e5e42ea1d35624e4930f40787705c031b0946a
Cecilie Skafte <cesk0001@stud.kea.dk>
2025-02-05 19:14:27 +0100

    produkt.js

--- productlist.js
+++ productlist.js
@@ -1,25 +1,31 @@
-const productContainer = document.querySelector(".productlistcontainer");
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+/*wtf?^ */
 
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
-  .then((response) => response.json())
-  .then((data) => showList(data));
+const productlistContainer = document.querySelector("#productlistcontainer");
+let url = "https://kea-alt-del.dk/t7/api/products/";
 
-function showList(products) {
-  console.log(products);
-  const markup = products.map
-    .map((product) => {
-      markup += `<div class="productlistcard">
-                <img class="productlistimage" src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="">
+function showProducts(data) {
+  const markup = data
+    .map(
+      (product) =>
+        `<div class="productlistcard${product.discount && "hasDiscount"} ${product.soldout && "isSoldOut"}">
+                <img class="productlistimage" src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="">
             </div>
-            <div class="productlistcontent">
+            <div class="productlistcontent">${product.id}"
                 <p class="productlistbrandname">${product.brandname}</p>
                 <h3 class="productlistdisplayname">${product.productdisplayname}</h3>
                 <p class="productlistprice">${product.price} DKK</p>
-                <p class="productlistdiscount"> ${product.discount}</p>
+                
                 <p class="productlistsoldout">Sold out!</p>
-            </div>`;
-    })
-    .join("");
-  console.log(markup);
-  listContainer.innerHTML = markup;
+            </div>`
+    )
+    .join(``);
+  productlistContainer.innerHTML = markup;
 }
+function hentData() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showProducts(data));
+}
+hentData();
