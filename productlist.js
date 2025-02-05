f601511382d8a6c8ebcb506a6c5d42bc925fb40c
Cecilie Skafte <cesk0001@stud.kea.dk>
2025-02-05 19:46:52 +0100

    Har fået productlist til at virke

--- productlist.js
+++ productlist.js
@@ -5,21 +5,31 @@ fetch(`https://kea-alt-del.dk/t7/api/products/`)
   .then((data) => showList(data));
 
 function showList(products) {
-  console.log(products);
-  const markup = products.map
+  console.log("Products:", products);
+  let outputHtml = "";
+  const markup = products
     .map((product) => {
-      markup += `<div class="productlistcard">
-                <img class="productlistimage" src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="">
-            </div>
-            <div class="productlistcontent">
-                <p class="productlistbrandname">${product.brandname}</p>
-                <h3 class="productlistdisplayname">${product.productdisplayname}</h3>
-                <p class="productlistprice">${product.price} DKK</p>
-                <p class="productlistdiscount"> ${product.discount}</p>
-                <p class="productlistsoldout">Sold out!</p>
-            </div>`;
+      console.log("Product:", product);
+      outputHtml += `
+      <div class="productlistcard">
+        <div class="productlistcontent">
+          <a href="produkt.html">
+            <img class="productlistimage" src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="">
+          </a>
+          <p class="productlistbrandname">${product.brandname}</p>
+          <h3 class="productlistdisplayname">${product.productdisplayname}</h3>
+          <p class="productlistprice">${product.price} DKK</p>
+          <p class="productlistdiscount">-${product.discount}%</p>
+          <p class="productlistsoldout">Sold out!</p>
+        </div>
+        <div class="productlist-button-wrapper">
+          <a class="productlist-button" href="produkt.html">
+              Read more
+          </a>
+        </div>
+      </div>`;
     })
     .join("");
-  console.log(markup);
-  listContainer.innerHTML = markup;
+  console.log(outputHtml);
+  productContainer.innerHTML = outputHtml;
 }
--- test.js
+++
@@ -1,8 +0,0 @@
-const liste = document.querySelector("#playliste");
-const mitArray = ["WAP", "Family Ties", "Helena", "Den Knaldrøde Gummibåd"];
-
-function lavListe() {
-  const punkter = mitArray.map((navn) => `<ul>${navn}</ul>`).join(``);
-  liste.innerHTML = punkter;
-}
-lavListe();
